6c9424f3fa80b7a8b09fb524c8e83b93d7d187e8
Bartosz Powidzki <bpowidzki@gmail.com>
2024-06-17 22:52:39 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheLostFortress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Room r = new Room();
+            r.name = "Troll Room";
+            r.n = 1;
+            r.s = 2;
+            r.w = 3;
+            r.e = 4;
+            outputTB.Text =  $"You are in the {r.name} There are exits to: ";
+        }
+    }
+}
---
+++ Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLostFortress
+{
+    public class Room
+    {
+        public int n;
+        public int s;
+        public int w;
+        public int e;
+        public string name;
+    }
+}
