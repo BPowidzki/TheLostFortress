90910d779afd3f14a719bf3a40783f6a210df03d
Bartosz Powidzki <bpowidzki@gmail.com>
2024-06-20 00:22:48 +0200

    Added Thing

---
+++ gameClasses/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLostFortress.gameClasses
+{
+    
+        public class Player : Thing
+        {
+            private Room _location; //Room where player is currently residing
+
+            public Player(string aName, string aDescription, Room aRoom) : 
+                base(aName, aDescription) 
+            {
+            _location = aRoom;
+            }
+            public Room Location
+            {
+                get => _location;
+                set => _location = value;
+            }
+        }
+}
---
+++ gameClasses/Thing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLostFortress.gameClasses
+{
+    public class Thing
+    {
+        private string _name;
+        private string _description;
+
+        public Thing(string name, string descriprion)
+        {
+            _name = name;
+            _description = descriprion;
+        }
+
+        public string Name // Name property
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Description // Description property
+        {
+            get => _description;
+            set => _description = value;
+        }
+
+    }
+}
